0969e8bb2acbec9935c25e54b3025de0a0fdd585
Justin Chen <Jchen210@asu.edu>
2016-07-02 19:45:11 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,6 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.modal-trigger').leanModal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
